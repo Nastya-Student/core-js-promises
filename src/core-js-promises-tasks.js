4b555ee228b78e8fc96f0fb9086cc01c8a07900c
Nastya Belova <nastjabelova0@gmail.com>
2026-01-31 00:40:35 +0300

    feat: implement getFirstResolvedPromiseResult

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -59,8 +59,12 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return (
+    Promise.allSettled(promises).then((results) =>
+      results.filter((result) => result.status === 'fulfilled')
+    )[0] ?? promises[0]
+  );
 }
 
 /**
